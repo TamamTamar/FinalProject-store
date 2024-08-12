a673246b3760de243eac40a8facbd3075cba8c49
TamamTamar <tamar.tamam770@gmail.com>
2024-08-12 21:04:22 +0300

    regi

---
+++ src/mock/register.ts
@@ -0,0 +1,20 @@
+ export const registerMock = {
+    "name": {
+        "first": "Moshe",
+        "middle": "",
+        "last": "Vis"
+    },
+    "phone": "0522345567",
+    "email": "MosheVis@gmail.com",
+    "password": "Abc!123Abc",
+   
+    "address": {
+        "state": "IL",
+        "country": "Israel",
+        "city": "Tel Aviv",
+        "street": "Shoham",
+        "houseNumber": 5,
+        "zip": "35"
+    },
+   
+} 
