72f3f0d9fe7361a67050f27f73f158f8ed9311e6
TamamTamar <113338791+TamamTamar@users.noreply.github.com>
2024-10-27 21:13:11 +0200

    Update AddToCartButton.tsx

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -1,45 +1,34 @@
 import { FC, useState } from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import './AddToCartButton.scss';
-import { AddToCartButtonProps, IVariant } from '../../@Types/productType';
-import useCart from '../../hooks/useCart';
-import dialogs from '../../ui/dialogs';
-import { useAuth } from '../../hooks/useAuth';
-import { useNavigate } from 'react-router-dom';
+import { AddToCartButtonProps, IVariant } from '../@Types/productType';
+import useCart from '../hooks/useCart';
+import dialogs from '../ui/dialogs';
+import { useAuth } from '../hooks/useAuth';
 
-const AddToCartButton: FC<AddToCartButtonProps> = ({ productId, variants, title, image, onNotLoggedIn }) => {
+const AddToCartButton: FC<AddToCartButtonProps> = ({ productId, variants, title, image }) => {
     const [selectedVariant, setSelectedVariant] = useState<IVariant | null>(variants[0] || null);
     const { addToCart } = useCart();
-    const { token } = useAuth(); // Assuming useAuth provides the user's authentication token
-    const navigate = useNavigate();
+    const { isLoggedIn } = useAuth();
 
     const handleAddToCart = async () => {
-        if (!token) {
-            if (onNotLoggedIn) {
-                onNotLoggedIn(); // Call the callback to handle the not-logged-in state
-            } else {
-                dialogs.error("Login Required", "You need to be logged in to add items to the cart. Please log in first.").then(() => {
-                    navigate('/login');
-                });
-            }
-            return;
-        }
-
         if (selectedVariant) {
             console.log("Adding product to cart:", selectedVariant);
             try {
-                await addToCart(productId, selectedVariant._id, 1, selectedVariant.size, selectedVariant.price);
+                await addToCart(productId, selectedVariant._id, 1, selectedVariant.size, selectedVariant.price, !isLoggedIn);
+                
                 dialogs.success(
                     "Product Added",
                     `<div style="display: flex; align-items: center;">
                         <img src="${image.url}" alt="${title}" style="width: 50px; height: 50px; object-fit: cover; margin-right: 10px;" />
                         <div>
-                            <p>${title} has been added to your cart.</p>
+                            <p>${title} has been added to your cart${!isLoggedIn ? " (Guest)" : ""}.</p>
                         </div>
                     </div>`
                 );
             } catch (error) {
                 console.error("Failed to add product to cart:", error);
+                dialogs.error("Error", "Failed to add the product to your cart.");
             }
         } else {
             console.error("No variant selected");
@@ -48,7 +37,7 @@ const AddToCartButton: FC<AddToCartButtonProps> = ({ productId, variants, title,
 
     return (
         <div className="add-to-cart-container">
-            <p>{selectedVariant && selectedVariant.quantity > 0 ? 'In Stock' : 'Out of Stock'}</p>
+            <p> {selectedVariant?.quantity > 0 ? 'In Stock' : 'Out of Stock'}</p>
             <div className="price-container" style={{ marginBottom: '20px', marginTop: '15px' }}>
                 <span className="original-price" style={{ marginRight: '10px' }}>
                     ${(selectedVariant?.price * 1.2).toFixed(2)}
